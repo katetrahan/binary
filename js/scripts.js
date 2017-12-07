2045abe4026d1efae7cbdaef51c4dd9b62063760
Kate Trahan and Natalia Telpukhova <kate@>
2017-12-06 16:45:55 -0800

    add spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 // Back End Logic
 var binaryToDecimal = function (number) {
-  return number;
+  debugger;
+  var tmp = number;
+  var binaryArray = tmp.split("").reverse();
+  var result = 0
+  // if (tmp !== 1 && tmp !== 0 ) {
+  //   return "weird";
+  // }
+  // else if ((tmp === 1) || (tmp === 0)) {
+  //   return tmp;
+  // }
+  for (var i= 0; i < binaryArray.length; i +=1) {
+    result += binaryArray[i]*Math.pow(2,i);
+  }
+  return result
 }
+
 
 
 
@@ -18,8 +32,14 @@ $(document).ready(function() {
   $("#binary").submit(function(event){
     event.preventDefault();
     var number = $("#number").val();
-    result = binaryToDecimal(number);
-    $("#result").text(result);
+    var result = binaryToDecimal(number);
+     if (result==="weird") {
+       $("#result").text("please enter in binary");
+     }
+     else {
+       $("#result").text(result);
+     }
+
 
   })
 });
